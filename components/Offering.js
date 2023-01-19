45d3217f2ca27117b8ba56c9bd39e14ec872646a
Lintang Aji Yoga Pratama <lintangajiyogapratama@gmail.com>
2023-01-19 17:49:08 +0700

    update

--- components/Offering.js
+++ components/Offering.js
@@ -35,19 +35,21 @@ export default function Offering() {
           </p>
         </div>
       </Link>
-      <div className="cursor-pointer hover:underline">
-        <div className="flex justify-center mx-auto items-center rounded-xl bg-blue-100 w-[50px] h-[50px]">
-          <Image
-            src="/img/choose-conselor.png"
-            width={40}
-            height={40}
-            alt="consultation"
-          />
+      <Link href="/conselor">
+        <div className="cursor-pointer hover:underline">
+          <div className="flex justify-center mx-auto items-center rounded-xl bg-blue-100 w-[50px] h-[50px]">
+            <Image
+              src="/img/choose-conselor.png"
+              width={40}
+              height={40}
+              alt="consultation"
+            />
+          </div>
+          <p className="text-center text-[11px] mt-1 max-w-[75px]">
+            Paket Konseling
+          </p>
         </div>
-        <p className="text-center text-[11px] mt-1 max-w-[75px]">
-          Paket Konseling
-        </p>
-      </div>
+      </Link>
     </div>
   );
 }
