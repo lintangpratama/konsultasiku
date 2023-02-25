4a2e5748846852ef3212e37c64146e932eff97c2
Lintang Aji Yoga Pratama <lintangajiyogapratama@gmail.com>
2023-02-25 13:32:56 +0700

    update

--- app/build/generated/source/buildConfig/release/com/konsultasiku/app/twa/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/com/konsultasiku/app/twa/BuildConfig.java
@@ -1,12 +1,12 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package app.netlify.konsultasiku.twa;
+package com.konsultasiku.app.twa;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "app.netlify.konsultasiku.twa";
+  public static final String APPLICATION_ID = "com.konsultasiku.app.twa";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "konsultasiku";
+  public static final String VERSION_NAME = "3";
 }
